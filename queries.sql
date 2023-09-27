10ebf3e1675bbc929a21a153911572793bb95960
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-09-27 13:01:45 +0430

    Selecting Challange completed

--- queries.sql
+++ queries.sql
@@ -1,3 +1,61 @@
 /*Queries that provide answers to the questions from all projects.*/
+SELECT
+    *
+FROM
+    animals
+WHERE
+    name LIKE '%mon';
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT
+    name
+FROM
+    animals
+WHERE
+    date_of_birth BETWEEN '2016-01-01'
+    AND '2019-12-31';
+
+SELECT
+    name
+FROM
+    animals
+WHERE
+    (neutered = true)
+    AND (escape_attempts < 3);
+
+SELECT
+    date_of_birth
+FROM
+    animals
+WHERE
+    (name = 'Augmon')
+    OR (name = 'Pikachu');
+
+SELECT
+    name,
+    escape_attempts
+FROM
+    animals
+WHERE
+    (weigth_kg > 10.5);
+
+SELECT
+    *
+FROM
+    animals
+WHERE
+    (neutered = true);
+
+SELECT
+    *
+FROM
+    animals
+WHERE
+    name <> 'Gabumon';
+
+SELECT
+    *
+FROM
+    animals
+WHERE
+    (weigth_kg >= 10.4)
+    AND (weigth_kg <= 17.3);
