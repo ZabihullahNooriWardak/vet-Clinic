b91a86c5abae41bde41b62c242610fdc9f758fc7
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-09-27 11:50:36 +0430

    data inserted

--- schema.sql
+++ schema.sql
@@ -1,9 +1,11 @@
 /* Database schema to keep the structure of entire database. */
- CREATE DATABASE vet_clinic;
+CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals(id INTEGER,
-name VARCHAR(100),
-date_of_birth DATE,
-escape_attempts INTEGER,
-neutered BOOLEAN,
-weigth_kg DECIMAL);
+CREATE TABLE animals(
+    id INTEGER,
+    name VARCHAR(100),
+    date_of_birth DATE,
+    escape_attempts INTEGER,
+    neutered BOOLEAN,
+    weigth_kg DECIMAL
+);
