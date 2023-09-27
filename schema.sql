80da625e4f0b3be82eb2529224b2e6ef9a7da473
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-09-27 11:31:13 +0430

    first step completed schema created

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
+ CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals(id INTEGER,
+name VARCHAR(100),
+date_of_birth DATE,
+escape_attempts INTEGER,
+neutered BOOLEAN,
+weigth_kg DECIMAL);
