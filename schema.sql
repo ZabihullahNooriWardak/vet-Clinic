43790622c730cf751b663a96e5e994854fd47c80
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-09-29 18:40:35 +0430

    schema.sql file edited (owner and species table added) and animals modified

--- schema.sql
+++ schema.sql
@@ -14,3 +14,42 @@ ALTER TABLE
     animals
 ADD
     COLUMN species text;
+
+CREATE TABLE owners(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    full_name TEXT,
+    age INTEGER
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT
+);
+
+ALTER TABLE
+    animals DROP COLUMN id;
+
+ALTER TABLE
+    animals
+ADD
+    COLUMN id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY;
+
+ALTER TABLE
+    animals
+ADD
+    COLUMN species_id INT;
+
+ALTER TABLE
+    animals
+ADD
+    COLUMN owner_id INT;
+
+ALTER TABLE
+    animals
+ADD
+    CONSTRAINT fk_species_id FOREIGN KEY (species_id) REFERENCES species (id);
+
+ALTER TABLE
+    animals
+ADD
+    CONSTRAINT fK_owner_id FOREIGN KEY (owner_id) REFERENCES owners (id);
